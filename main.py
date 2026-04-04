bd80b4ab306135962613890d830c916070622d97
ofthewildfire <103148299+ofthewildfire@users.noreply.github.com>
2026-04-04 23:01:45 +0200

    Added circleshape.py for sprites in game

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+from pygame.time import Clock
 
 import constants
 from logger import log_state
@@ -8,6 +9,9 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
 
+    pygame.time.Clock()
+    dt = 0
+
     while True:
         log_state()
         for event in pygame.event.get():
@@ -15,6 +19,8 @@ def main():
                 return
             screen.fill("black")
             pygame.display.flip()
+        dt = Clock().tick(60) / 1000
+        print(dt)
 
     # print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     # print(f"Screen width: {constants.SCREEN_WIDTH}")
